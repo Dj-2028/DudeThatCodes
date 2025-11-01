d65a59d4339103497785ace4c226cc6e1e1806e1
Diksha Jain <dikshajain2026@gmail.com>
2025-11-01 19:51:46 +0530

    improved ui

--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -3,13 +3,15 @@ import { useState } from 'react'
 import MobileSection from '../Components/MobileSection'
 import Solutions from '../Components/Solutions'
 import HeroSection from '../Components/HeroSection'
+import Navbar from '../Components/Navbar'
 
 const Home = () => {
 
 
   return (
     <>
-      <div className='bg-[#00111F] pt-[2.5vh]'>
+      <div className='bg-[#00111F]'>
+        <Navbar />
         <HeroSection />
         <MobileSection />
         <div className='p-20' >
